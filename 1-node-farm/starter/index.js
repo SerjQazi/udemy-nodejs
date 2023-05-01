7137d42e6162c2a1a65cbcbffdd99cf9da1746c8
Serj Qazi <serjqazi@Serjs-MacBook-Pro.local>
2023-04-30 20:22:21 -0400

    Day 2, ...

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,10 @@
 const fs = require('fs');
+const http = require('http');
+const path = require('path');
+const url = require('url');
+
+////////////////////////////////////////
+// FILES
 
 // Blocking, synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -8,22 +14,56 @@ const fs = require('fs');
 // fs.writeFileSync ('./txt/output.txt', textOut);
 // console.log('File Written!');
 
-// Non-blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  // error (err) handling
-  if (err) return console.log ('❌ ERROR!💢 ❌')
+// // Non-blocking, asynchronous way
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   // error (err) handling
+//   if (err) return console.log ('❌ ERROR!💢 ❌')
 
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    // console.log(data2);
-    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-      // console.log(data3);
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     // console.log(data2);
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//       // console.log(data3);
 
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
-        console.log('Your file has been written... 🤪')
-      })
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+//         console.log('Your file has been written... 🤪')
+//       })
+//     });
+//   });
+// });
+
+// // this will display first, while we are waiting for the file to be read.
+// console.log('Will read file... !')
+
+
+
+////////////////////////////////////////
+// SERVER
+
+// Synchronies JSON file reading. This stops file from reading at every request
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data)
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if(pathName === '/' || pathName === '/overview') {
+    res.end('This is the OVERVIEW!');
+
+  } else if(pathName === '/product') {
+    res.end('This is the PRODUCT!');
+
+  } else if (pathName === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' })
+    res.end(data);
+
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html'
     });
-  });
+    res.end('<h1>Page not found!<h1>');
+  }
 });
 
-// this will display first, while we are waiting for the file to be read.
-console.log('Will read file... !')
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000...');
+})
