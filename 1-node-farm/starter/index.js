178ac5f715be5635fc96dbe66a1e0729d0fd21c8
Serj Qazi <serjqazi@Serjs-MacBook-Pro.local>
2023-05-02 21:22:37 -0400

    parsing variables from url and modules

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const path = require('path');
 const url = require('url');
 
+const replaceTemplate = require('./modules/replaceTemplate');
+
 ////////////////////////////////////////
 // FILES
 
@@ -39,24 +41,16 @@ const url = require('url');
 ////////////////////////////////////////
 // SERVER
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nuitrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  return output;
-};
-
 // Synchronies JSON file reading. This stops file from reading at every request
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`, 'utf-8'
+  );
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`, 'utf-8'
+  );
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`, 'utf-8'
+  );
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data)
@@ -64,10 +58,10 @@ const dataObj = JSON.parse(data)
 
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
   // Overview page
-  if(pathName === '/' || pathName === '/overview') {
+  if(pathname === '/' || pathname === '/overview') {
     res.writeHead(200, { 'Content-type': 'text/html' })
 
     const cardsHtml = dataObj.map(currentElement => replaceTemplate(tempCard, currentElement)).join('');
@@ -75,15 +69,16 @@ const server = http.createServer((req, res) => {
     res.end(output);
 
 
-
-
     // Product page
-  } else if(pathName === '/product') {
-    res.end('This is the PRODUCT!');
+  } else if(pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' })
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
 
     // API
-  } else if (pathName === '/api') {
+  } else if (pathname === '/api') {
     res.writeHead(200, { 'Content-type': 'application/json' })
     res.end(data);
 
